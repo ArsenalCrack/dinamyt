59273a10634d72ea16d0554d0e3ce13dff1fe3f3
ArsenalCrack <AmirSarmiento0430gmail.com4>
2026-01-03 14:43:52 -0500

    acomodando el logo icon y el inicio del index

--- frontend/src/app/shared/components/footer/footer.component.ts
+++ frontend/src/app/shared/components/footer/footer.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ApiService } from '../../../core/services/api.service'; // Ajusta la ruta si es necesario
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit {
 
+  private api = inject(ApiService);
+  estadoBackend: string = 'Comprobando...';
+
+  ngOnInit() {
+    this.api.getSaludo().subscribe({
+      next: (datos) => { this.estadoBackend = datos.estado; },
+      error: () => { this.estadoBackend = 'Offline'; }
+    });
+  }
 }
