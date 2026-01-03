67814b1189f4121aa6d232b171c5feeffa750704
ArsenalCrack <AmirSarmiento0430gmail.com4>
2026-01-03 17:40:03 -0500

    acomodando index del sitio web

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+// Asegúrate de que la ruta del import sea correcta según tus carpetas
+import { HomeComponent } from './features/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  // Cuando la ruta está vacía (localhost:4200), muestra el Home
+  { path: '', component: HomeComponent },
+
+  // (Opcional) Si escriben una ruta loca, redirigir al Home
+  { path: '**', redirectTo: '' }
+];
