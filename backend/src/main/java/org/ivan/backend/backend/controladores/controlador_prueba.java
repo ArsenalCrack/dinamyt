e926e4f93f0dcbd3bbfcae44afa57b5608ba291b
ivan <andresivan0807@gmail.com>
2026-01-03 16:07:38 -0500

    a

--- backend/src/main/java/org/ivan/backend/backend/controladores/controlador_prueba.java
+++ backend/src/main/java/org/ivan/backend/backend/controladores/controlador_prueba.java
@@ -2,11 +2,12 @@ package org.ivan.backend.backend.controladores;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class controlador_prueba {
     @GetMapping("/")
-    public void prueba(){
-
+    public String prueba(){
+        return "hola";
     }
 }
