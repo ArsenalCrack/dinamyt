6ce10ede5e05341d549bb7929fba22b1485e46e7
ivan <andresivan0807@gmail.com>
2026-01-06 18:24:13 -0500

    a

--- bd/dinamyt.sql
+++ bd/dinamyt.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 03-01-2026 a las 22:06:35
+-- Tiempo de generación: 07-01-2026 a las 00:23:40
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.0.30
 
@@ -28,10 +28,34 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `usuario` (
-  `ID` int(11) NOT NULL,
-  `nombre` int(11) NOT NULL,
-  `correo` int(11) NOT NULL
+  `ID_documento` bigint(20) NOT NULL,
+  `nombreC` varchar(150) NOT NULL,
+  `sexo` varchar(20) DEFAULT NULL,
+  `fecha_nacimiento` date DEFAULT NULL,
+  `cinturon_rango` varchar(20) DEFAULT NULL,
+  `Nacionalidad` varchar(20) DEFAULT NULL,
+  `Correo` varchar(120) DEFAULT NULL,
+  `Contraseña` varchar(255) DEFAULT NULL,
+  `numero_celular` int(50) DEFAULT NULL,
+  `verificacion` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `usuario`
+--
+
+INSERT INTO `usuario` (`ID_documento`, `nombreC`, `sexo`, `fecha_nacimiento`, `cinturon_rango`, `Nacionalidad`, `Correo`, `Contraseña`, `numero_celular`, `verificacion`) VALUES
+(123456, 'Andres Ivan', 'Masculino', '2022-01-04', NULL, 'espana', 'andresivan0807@gmail.com', 'Andres@2', NULL, 603188);
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`ID_documento`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
